53b7b9c7e96ad700ef68d398efdc81cd640d9ece
VilokMasuti <119694262+VilokMasuti@users.noreply.github.com>
2024-01-31 15:30:53 +0530

    done

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Add from "./Components/Add";
+import Simple from "./Components/Simple";
+
+function App() {
+  return (
+    <>
+      <div className=" w-full h-screen bg-amber-200 text-center flex items-center justify-center ">
+        <Add />
+        <Simple />
+      </div>
+    </>
+  );
+}
+
+export default App;
